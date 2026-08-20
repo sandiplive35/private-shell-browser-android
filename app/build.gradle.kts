30e3e254bc78d1e859824fc21c1ece34df1e8824
sandiplive35 <sandipsudipff@gmail.com>
2026-08-20 09:04:10 +0530

    build: configure Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.sandiplive.privateshell"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.sandiplive.privateshell"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.webkit:webkit:1.17.0")
+}
